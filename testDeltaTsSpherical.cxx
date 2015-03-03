e7cb1c2aec28ed866dd9e7a823f7bff39cf656fd
strutt <strutt@users.noreply.github.com>
2015-03-03 15:56:34 +0000

    Tidied up functions in CrossCorrelator. Updated testDeltaTSpherical.

--- testDeltaTsSpherical.cxx
+++ testDeltaTsSpherical.cxx
@@ -14,37 +14,20 @@ int main(int argc, char *argv[]){
   TApplication* theApp = new TApplication("App", &argc, argv);
   gROOT->ProcessLine(".x ~/.rootlogon.C");
   
-  /* Consider two antennas on middle ring */
+
   
-  Int_t ant1 = 16;
-  Int_t ant2 = 19;
+
 
   const int numRVals = 4;
-  Double_t rVals[numRVals] = {10, 100, 1000, 1000};
+  Double_t rVals[numRVals] = {5, 6, 7, 8};
   TH2D* hDts[numRVals] = {NULL};
 
   CrossCorrelator* cc = new CrossCorrelator();
+  cc->correlateEventTest(30, -15, 7);
 
-  for(Int_t rInd = 0; rInd<numRVals; rInd++){
-    Double_t rWave = rVals[rInd];    
-    TString name = TString::Format("hDt%d", rInd);
-    hDts[rInd] = new TH2D(name, name,
-			  NUM_BINS_PHI*NUM_PHI, cc->phiArrayDeg[0]-PHI_RANGE/2, cc->phiArrayDeg[15]+PHI_RANGE/2,
-			  NUM_BINS_THETA, -THETA_RANGE/2, THETA_RANGE/2);
-
-    for(Int_t phiSector = 0; phiSector<NUM_PHI; phiSector++){
-      for(Int_t phiInd = 0; phiInd < NUM_BINS_PHI; phiInd++){
-	Int_t phiBin = phiSector*NUM_BINS_PHI + phiInd;
-	Double_t phiDeg = -0.5*PHI_RANGE + phiBin*Double_t(PHI_RANGE)/NUM_BINS_PHI;
-	Double_t phiWave = TMath::DegToRad()*phiDeg;
-	for(Int_t thetaBin = 0; thetaBin < NUM_BINS_THETA; thetaBin++){
-	  Double_t thetaDeg = THETA_RANGE*((Double_t)thetaBin/NUM_BINS_THETA - 0.5);
-	  Double_t thetaWave = TMath::DegToRad()*thetaDeg;
-	  Int_t offset = cc->getDeltaTExpectedSpherical(ant1, ant2, phiWave, thetaWave, rWave);
-	  hDts[rInd]->SetBinContent(phiBin, thetaBin, offset);
-	}
-      }
-    }
+  for(Int_t rInd = 0; rInd<numRVals; rInd++){    
+    hDts[rInd] = cc->makeImageSpherical(AnitaPol::kVertical, rVals[rInd]);
+    hDts[rInd]->SetName(TString::Format("hSpherical%d", rInd));
   }
 
   TCanvas* c1 = new TCanvas();
