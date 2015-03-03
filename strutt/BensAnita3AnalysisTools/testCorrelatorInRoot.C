d05230686cb132b3a4b6bf0e430d645dc30cad90
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2015-03-03 12:31:02 +0000

    Updated testCorrelatorInRoot to draw a puler event. Updated Makefile to avoid linking against FFTtools and no longer using them in this repo. Added my spherical deltaT tests, because... why not.

--- strutt/BensAnita3AnalysisTools/testCorrelatorInRoot.C
+++ strutt/BensAnita3AnalysisTools/testCorrelatorInRoot.C
@@ -4,13 +4,11 @@
   /* Tedious loading of libraries to make this ROOT macro work... */
 
   gSystem->Load("libAnitaEvent.so"); // load event reader ROOT into CINT
-  // gSystem->Load("libMinuit.so"); // required by libAnitaCorrelator...
-  // gSystem->Load("libAnitaCorrelator.so"); // load Ryan/Matt Mottram's correlator into CINT  
 
   // Load a few prerequisite libraries into CINT...
   gSystem->Load("libMathMore.so"); // prereq for Ryan's ROOT FFTW wrapper
   gSystem->Load("libfftw3.so"); // prereq for Ryan's ROOT FFTW wrapper
-  gSystem->Load("libRootFftwWrapper.so"); // Ryan's ROOT FFTW wrapper, which is a prereq for Cross Correlator
+  gSystem->Load("libRootFftwWrapper.so"); // Ryan's ROOT FFTW wrapper, required by EventReaderRoot
 
   gSystem->Load("libBensAnitaTools.so"); // load my correlator and other lovely things into CINT
 
@@ -18,7 +16,7 @@
 
 
   TString dataPath = "~/UCL/ANITA/anita3Data/flight1415/telem/";
-  const Int_t run = 335;
+  const Int_t run = 158;
 
 
 
@@ -36,33 +34,32 @@
   RawAnitaHeader* header = NULL;
   tHead->SetBranchAddress("header", &header);
 
-  // In telem data the matching of events in eventHeadFile and eventFile isn't necessarily perfect.
-  // Need to take account of that by building another set of indices with event number.
-  tEvent->BuildIndex("event->eventNumber");
+
+  UInt_t eventIWant = 10106161;
+  Long64_t entryIWant = -1;
+  Long64_t numEntries = tHead->GetEntries();
+  for(Long64_t entry=0; entry<numEntries; entry++){
+    tHead->GetEntry(entry);
+    if(header->eventNumber == eventIWant){
+      entryIWant = entry;
+    }
+  }
+
+  tHead->GetEntry(entryIWant);
+  tEvent->GetEntry(entryIWant);
 
   // Make cross correlator
   CrossCorrelator* cc = new CrossCorrelator();
-
-
   
-  // Loop over data...
-
-  Long64_t numEntries = tHead->GetEntries();
-  for(Long64_t entry=0; entry<numEntries; entry++){
-
-    // Get matching event numbered entries from header and event files
-    tHead->GetEntry(entry);
-    Int_t eventIndex = tEvent->GetEntryWithIndex(header->eventNumber);
-
-    UsefulAnitaEvent* usefulEvent = new UsefulAnitaEvent(rawEvent, WaveCalType::kDefault, header);
+  UsefulAnitaEvent* usefulEvent = new UsefulAnitaEvent(rawEvent, WaveCalType::kDefault, header);
     
-    cc->correlateEvent(usefulEvent); // Generates set of cross correlations
+  cc->correlateEvent(usefulEvent); // Generates set of cross correlations
+  
+  // TH2D* hImage = cc->makeImage(AnitaPol::kVertical); // then generate and image
+  TH2D* hImage = cc->makeImage(AnitaPol::kHorizontal); // then generate and image
+  hImage->SetTitle("A pulser event reconstructed by Cross Correlator");
+  hImage->SetName(TString::Format("hImage%u", eventIWant));
     
-    TH2D* hImage = cc->makeImage(AnitaPol::kVertical); // then generate and image
-    
-    hImage->Draw("colz"); // ... and enjoy it, isn't it pretty?
-    break;
-  }
-
+  hImage->Draw("colz"); // ... and enjoy it, isn't it pretty?
 
 }
