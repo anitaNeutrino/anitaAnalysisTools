0c16d988d399f8a9429c37f769a5a0137f10a7d8
strutt <strutt@users.noreply.github.com>
2015-10-30 11:56:45 +0000

    Added OutputConvention class to abstract file name production by analysis programs.

---
+++ OutputConvention.h
@@ -0,0 +1,46 @@
+/* -*- C++ -*-.***************************************************************************************************
+ Author: Ben Strutt
+ Email: b.strutt.12@ucl.ac.uk
+
+ Description: 
+             A class to manage output file names from environment variables, exec arguments and time.
+*************************************************************************************************************** */
+
+#ifndef OUTPUTCONVENTION_H
+#define OUTPUTCONVENTION_H
+
+
+#include "TDatime.h"
+#include "TString.h"
+#include "TFile.h"
+
+
+class OutputConvention{
+
+public:
+
+  OutputConvention(int argcIn, char* argvIn[]);
+  ~OutputConvention();
+
+  TString getOutputFileName();
+
+  
+private:
+  int argc;
+  char** argv;
+  TDatime dateTime;
+  TString outputDir;
+  TString dateTimeSuffix;
+  TString outFileName;
+  
+  TString getDateTimeSuffix();
+  TString getOutputDir();
+
+  
+  
+};
+
+
+
+
+#endif
---
+++ testOutputConvention.cxx
@@ -0,0 +1,24 @@
+/* -*- C++ -*-.***************************************************************************************************
+ Author: Ben Strutt
+ Email: b.strutt.12@ucl.ac.uk
+
+ Description: 
+             Just for fun.
+*************************************************************************************************************** */
+
+#include "OutputConvention.h"
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+  
+
+  OutputConvention oc(argc, argv);
+
+  TString outFileName = oc.getOutputFileName();
+
+  TFile* f = new TFile(outFileName, "recreate");
+  f->Close();
+  
+  
+  return 0;
+}
