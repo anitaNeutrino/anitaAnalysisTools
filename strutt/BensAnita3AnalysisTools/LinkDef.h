dcb2566f8a60c3d1e54995b9cdb1c571d06f2897
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2016-03-14 20:28:49 +0000

    Including Linkdef and Doxyfile

--- strutt/BensAnita3AnalysisTools/LinkDef.h
+++ strutt/BensAnita3AnalysisTools/LinkDef.h
@@ -7,8 +7,7 @@
 #pragma link C++ class CrossCorrelator+;
 #pragma link C++ class FancyTTreeInterpolator+;
 #pragma link C++ namespace RootTools+;
-#pragma link C++ class FancyFFTs+;
-#pragma link C++ namespace PowSpecNorm+;
+#pragma link C++ namespace FancyFFTs+;
 #pragma link C++ class ProgressBar+;
 #pragma link C++ class AveragePowerSpectrum+;
 #pragma link C++ class AnitaAveragePowerSpectrum+;
