c21d420e1d9ca0f8320fee266a75e31f5a8e1db7
Ben Strutt <strutt@physics.ucla.edu>
2017-05-11 16:17:21 -0700

    Add drivers to run over data using Cosmin's SineSubtract filter

---
+++ drivers/filterDecimatedSineSub.cxx
@@ -0,0 +1,30 @@
+#include "AnalysisFlow.h"
+#include "BasicFilters.h"
+#include "AcclaimFilters.h"
+#include "UCFilters.h"
+
+using namespace Acclaim;
+
+int main(int argc, char* argv[]){
+
+  AnitaVersion::set(3);
+  int run = argc > 1 ? atoi(argv[1]) : 352;
+
+  std::map<TString, FilterStrategy*> filterStrats;
+  bool saveOutput = false;
+  Filters::appendFilterStrategies(filterStrats, saveOutput);
+  
+  // FilterStrategy* strat = Filters::findStrategy(filterStrats, "RayleighFilter");
+  FilterStrategy* strat = new FilterStrategy();
+  UCorrelator::fillStrategyWithKey(strat,"sinsub_5_3_ad_2");
+
+  for(unsigned i=0; i < strat->nOperations(); i++){
+    std::cout << i << "\t" << strat->getOperation(i)->description() << std::endl;
+  }
+  // return 0;
+  
+  AnalysisFlow analysis(argv[0], run, AnalysisFlow::kDecimated, strat, BlindDataset::kDefault);
+  analysis.doAnalysis();
+    
+  return 0;
+}
