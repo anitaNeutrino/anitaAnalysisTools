e5bd1b1deafdd0c7e0da938dfea6c3f951f6210c
Ben Strutt <strutt@physics.ucla.edu>
2017-08-24 09:13:47 -0700

    Pass extra subdivision variables to MC driver scripts

--- drivers/filterAllSineSubPlusBrickWall.cxx
+++ drivers/filterAllSineSubPlusBrickWall.cxx
@@ -9,6 +9,8 @@ int main(int argc, char* argv[]){
 
   AnitaVersion::set(3);
   int run = argc > 1 ? atoi(argv[1]) : 352;
+  int division = argc > 3? atoi(argv[2]) : 0;
+  int numDivisions = argc > 3? atoi(argv[3]) : 1;  
 
   std::map<TString, FilterStrategy*> filterStrats;
   bool saveOutput = false;
@@ -31,7 +33,7 @@ int main(int argc, char* argv[]){
   
   // return 0;
   
-  AnalysisFlow analysis(argv[0], run, AnalysisFlow::kAll, strat, AnitaDataset::kDefault);
+  AnalysisFlow analysis(argv[0], run, AnalysisFlow::kAll, strat, AnitaDataset::kDefault, division, numDivisions);
   analysis.doAnalysis();
     
   return 0;
