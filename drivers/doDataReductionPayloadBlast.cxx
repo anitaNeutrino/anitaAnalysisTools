875b08d6cbdaa66340607497a0127e34751c92d4
Ben Strutt <strutt@physics.ucla.edu>
2017-10-25 08:11:41 -0700

    Add driver to make a reduced tree purely of payload blasts

---
+++ drivers/doDataReductionPayloadBlast.cxx
@@ -0,0 +1,33 @@
+#include "AnalysisCuts.h"
+#include "SumTreeReductionSelector.h"
+#include "SummarySet.h"
+#include "OutputConvention.h"
+#include <iostream>
+#include "TSystem.h"
+
+using namespace Acclaim;
+
+int main(int argc, char* argv[]){
+
+  if(argc != 2){
+    std::cerr << argv[0] << " 'glob' " << std::endl;
+    return 1;
+  }
+  OutputConvention oc(1, argv);  
+  const char* glob = argv[1];
+
+  AnalysisCuts::setMode(AnalysisCuts::kAcclaimAnalysis); // not that this should matter
+
+  SummarySet ss(glob);
+  ss.SetUseProof(true);
+
+  SumTreeReductionSelector reduce(oc.getOutputFileName(), "sumTree");
+
+  reduce.addEventSelectionCut(&AnalysisCuts::isTaggedAsPayloadBlast);
+
+  ss.Process(&reduce);
+  
+  gSystem->Exit(0); /// Must be called after initializing PROOF!
+
+  return 0;
+}
