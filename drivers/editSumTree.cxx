2e6a602bec29ac52c1f54b51f7b4bcd92a887ae2
Ben Strutt <strutt@physics.ucla.edu>
2017-11-30 15:38:13 -0800

    Quick check this doesn't select WAIS pulses

--- drivers/editSumTree.cxx
+++ drivers/editSumTree.cxx
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]){
     return 1;
   }
 
-  std::vector<UInt_t> eventNumbers {26323378, 27569662, 27897245, 28254843, 28627679, 28776748, 28930237, 28934295, 29006913, 29008144, 32879955, 32953817, 34745811, 34757629, 35020101, 35116217, 35118197, 36710262, 37009416, 37009533, 37009810, 38273261, 38619569, 39464799, 40633613, 40646231, 40996918, 41492058, 41654702, 41799439, 42256877, 42699600, 44675867, 45985415, 47512906, 47831713, 48225288, 48242449, 49313992, 50827460, 51950538, 53275871, 55496464, 55504417, 55861013, 56463512, 56829451, 56894100, 57108465, 57471113, 57519585, 57523926, 57654756, 57839882, 57948519, 58002861, 58008286, 58110716, 58226572, 58310817, 58326495, 58332241, 58421469, 59967256, 60072506, 61104509, 61173194, 61215330, 61238684, 61497702, 61534104, 61784557, 61876418, 62235301, 62235933, 62420819, 62732869, 62849802, 63024583, 63031985, 63484004, 63714468, 63845258, 63845261};
+  // std::vector<UInt_t> eventNumbers {26323378, 27569662, 27897245, 28254843, 28627679, 28776748, 28930237, 28934295, 29006913, 29008144, 32879955, 32953817, 34745811, 34757629, 35020101, 35116217, 35118197, 36710262, 37009416, 37009533, 37009810, 38273261, 38619569, 39464799, 40633613, 40646231, 40996918, 41492058, 41654702, 41799439, 42256877, 42699600, 44675867, 45985415, 47512906, 47831713, 48225288, 48242449, 49313992, 50827460, 51950538, 53275871, 55496464, 55504417, 55861013, 56463512, 56829451, 56894100, 57108465, 57471113, 57519585, 57523926, 57654756, 57839882, 57948519, 58002861, 58008286, 58110716, 58226572, 58310817, 58326495, 58332241, 58421469, 59967256, 60072506, 61104509, 61173194, 61215330, 61238684, 61497702, 61534104, 61784557, 61876418, 62235301, 62235933, 62420819, 62732869, 62849802, 63024583, 63031985, 63484004, 63714468, 63845258, 63845261};
  
   TString inDir = argv[1];
   TString inFileBaseName = argv[2];
@@ -48,20 +48,17 @@ int main(int argc, char* argv[]){
     settings.apply(dynamic_cast<TObject*>(&reco));
     settings.write(fOut);
     
-    ProgressBar p(eventNumbers.size());
-    Int_t lastRun = -1;
-    // for(Long64_t entry=0; entry < N; entry++){
-    for(UInt_t eventInd=0; eventInd < eventNumbers.size(); eventInd++){
-      ss.getEvent(eventNumbers.at(eventInd));
+    ProgressBar p(N);
+    // ProgressBar p(eventNumbers.size());
+    for(Long64_t entry=0; entry < N; entry++){
+      ss.getEntry(entry);
+
+    // for(UInt_t eventInd=0; eventInd < eventNumbers.size(); eventInd++){
+      // ss.getEvent(eventNumbers.at(eventInd));
       AnitaEventSummary* inSum = ss.summary();
       
-      if(inSum->run != lastRun){
-	lastRun = inSum->run;
-	std::cout << "on " << lastRun << std::endl;
-      }
-      
-      if(std::find(eventNumbers.begin(), eventNumbers.end(), inSum->eventNumber)!=eventNumbers.end()){
-
+      // if(std::find(eventNumbers.begin(), eventNumbers.end(), inSum->eventNumber)!=eventNumbers.end()){
+      if(inSum->flags.isPayloadBlast || inSum->flags.pulser > 0){
 
 
 	if(d && d->getCurrRun() != inSum->run){
@@ -119,8 +116,8 @@ int main(int argc, char* argv[]){
 
       }
 
-      // p.inc(entry, N);
-      p.inc(eventInd, N);      
+      p.inc(entry, N);
+      // p.inc(eventInd, N);
     }
     fOut->Write();
     fOut->Close();
