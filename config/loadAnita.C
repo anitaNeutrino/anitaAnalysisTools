3a7ab1bef0f64ff41a99c8e6b1080373e116a3a8
qabarr <qabarr@gmail.com>
2020-10-15 17:43:27 -0400

    Look in the right directory

--- config/loadAnita.C
+++ config/loadAnita.C
@@ -3,15 +3,15 @@
   // so we don't get things left in ROOT memory
   {
     const int nLibs = 9;
-    const char* libs[nLibs] = {"libRootFftwWrapper.so",
-			       "libAntarcticaRoot.so",
-			       "libAnitaIceMC.so",
-                               "libAnitaEvent.so",
-                               "libAnitaCorrelator.so",
-                               "libAnitaAnalysis.so",
-                               "libAnitaAnalysisTools.so",
-                               "libUCorrelator.so",
-                               "libAnitaMagicDisplay.so"};
+    const char* libs[nLibs] = {"${ANITA_UTIL_INSTALL_DIR}/lib/libRootFftwWrapper.so",
+			       "${ANITA_UTIL_INSTALL_DIR}/lib/libAntarcticaRoot.so",
+			       "${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaIceMC.so",
+                               "${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaEvent.so",
+                               "${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaCorrelator.so",
+                               "${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaAnalysis.so",
+                               "${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaAnalysisTools.so",
+                               "${ANITA_UTIL_INSTALL_DIR}/lib/libUCorrelator.so",
+                               "${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaMagicDisplay.so"};
     for(int i=0; i < nLibs; i++){
       TString loaded = gSystem->GetLibraries();
       if(!loaded.Contains(libs[i])){
