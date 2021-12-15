9486031734d7f40c4efa05ab35e43b1f1c2aca29
jwruss <jrussell09@gmail.com>
2021-12-14 18:32:56 -1000

    Next step in diagnostic, let's see what happens when I comment out the problematic line.

--- src/AcclaimClustering.cxx
+++ src/AcclaimClustering.cxx
@@ -1646,7 +1646,7 @@ void Acclaim::Clustering::LogLikelihoodMethod::doPathEventClustering(){
             
             std::cout << matchedCluster << " " << matchedCluster2 << std::endl;
 
-            if (!RootTools::vectorContainsValue(matchedClusters[z][matchedCluster], matchedCluster2)) matchedClusters[z][matchedCluster].push_back(matchedCluster2);
+//            if (!RootTools::vectorContainsValue(matchedClusters[z][matchedCluster], matchedCluster2)) matchedClusters[z][matchedCluster].push_back(matchedCluster2);
           }
         }	
       }
