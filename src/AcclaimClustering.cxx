5d950cc6f6b3171fead37eaa118c0ef64c586dc1
jwruss <jrussell09@gmail.com>
2022-05-05 23:00:53 -1000

    Type cast change to address error message.

--- src/AcclaimClustering.cxx
+++ src/AcclaimClustering.cxx
@@ -1369,6 +1369,7 @@ void Acclaim::Clustering::LogLikelihoodMethod::readInPathList(){
       for(UInt_t clusterInd=0; clusterInd < BaseList::getNumPaths(); clusterInd++){
         const BaseList::path& path = BaseList::getPath(clusterInd);
         clusters.at(z).push_back(Cluster(path, clusters.at(z).size(), 0, 90, 0));
+//        clusters.at(z).push_back(Cluster(path, clusters.at(z).size()));
         clusters.at(z).back().llEventCutInd = z;
         clusters.at(z).back().llEventCut = llEventCuts.at(z);
       }
